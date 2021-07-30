50e3baef183ac2d55a75aa8e8dd67b684dc207ea
maxwillias <willias_junior@hotmail.com>
2021-07-30 13:21:08 -0300

    parte de migrations feita

--- Trabalho_2/WebAppIntermediario/admin.py
+++ Trabalho_2/WebAppIntermediario/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Produto, Cliente
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome','preco','estoque')
 
 # Register your models here.
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente)
+
